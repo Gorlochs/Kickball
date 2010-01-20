22a0f5dba6e925be705aa9b17792044c0356b14a
shawn <shawn@73bdbd85-dd75-0410-9d14-8527f93b1508>
2010-01-20 00:49:34 +0000

    graphic changes
    git-svn-id: http://literalshore.com/svn/kickball/trunk@280 73bdbd85-dd75-0410-9d14-8527f93b1508

--- Classes/FriendRequestsViewController.m
+++ Classes/FriendRequestsViewController.m
@@ -66,8 +66,8 @@
             cell.textLabel.text = ((FSUser*)[friendRequests objectAtIndex:indexPath.row]).firstnameLastInitial;
             
             UIButton* btn = [UIButton buttonWithType:UIButtonTypeCustom];
-            [btn setImage:[UIImage imageNamed:@"profileCheckin01.png"] forState:UIControlStateNormal];
-            [btn setImage:[UIImage imageNamed:@"profileCheckin02.png"] forState:UIControlStateHighlighted];
+            [btn setImage:[UIImage imageNamed:@"add01.png"] forState:UIControlStateNormal];
+            [btn setImage:[UIImage imageNamed:@"add02.png"] forState:UIControlStateHighlighted];
             btn.frame = CGRectMake(-40, 0, 80, 15);
             btn.userInteractionEnabled = YES;
             btn.tag = indexPath.row;
