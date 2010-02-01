f8289b7f8ae7d008f249a0c03eefbfbb8c1954db
shawn <shawn@73bdbd85-dd75-0410-9d14-8527f93b1508>
2010-02-01 06:35:39 +0000

    changed copy
    git-svn-id: http://literalshore.com/svn/kickball/trunk@406 73bdbd85-dd75-0410-9d14-8527f93b1508

--- Classes/PlaceDetailViewController.m
+++ Classes/PlaceDetailViewController.m
@@ -270,7 +270,7 @@
         } else if ([[self getSingleCheckin].mayor.mayorTransitionType isEqualToString:@"stolen"] || [[self getSingleCheckin].mayor.mayorTransitionType isEqualToString:@"new"]) {
             if ([[self getSingleCheckin].mayor.mayorTransitionType isEqualToString:@"stolen"]) {
                 //newMayorshipLabel.text = [NSString stringWithFormat:@"%@ (Crown stolen from %@)", [self getSingleCheckin].mayor.mayorCheckinMessage, [self getSingleCheckin].mayor.user.firstnameLastInitial];
-                newMayorshipLabel.text = [NSString stringWithFormat:@"Congrats! %@ is yours with %d check-ins and %@ lost her crown.", 
+                newMayorshipLabel.text = [NSString stringWithFormat:@"Congrats! %@ is yours with %d check-ins and %@ lost their crown.", 
                                           [self getSingleCheckin].venue.name, 
                                           [self getSingleCheckin].mayor.numCheckins, 
                                           [self getSingleCheckin].mayor.user.firstnameLastInitial];
