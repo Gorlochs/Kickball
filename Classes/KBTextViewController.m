e96ace26031e31943b4923b1c089075497cd4a8f
shawn <shawn@73bdbd85-dd75-0410-9d14-8527f93b1508>
2010-03-03 03:48:14 +0000

    ticket 223: changed DONE to return and allow carriage returns
    git-svn-id: http://literalshore.com/svn/kickball/trunk@528 73bdbd85-dd75-0410-9d14-8527f93b1508

--- Classes/KBTextViewController.m
+++ Classes/KBTextViewController.m
@@ -170,21 +170,11 @@
 	// e.g. self.myOutlet = nil;
 }
 
-- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
-    // Any new character added is passed in as the "text" parameter
-    if ([text isEqualToString:@"\n"]) {
-        // Be sure to test for equality using the "isEqualToString" message
-        [textView resignFirstResponder];
-        //[self shout];
-        // Return FALSE so that the final '\n' character doesn't get added
-        return FALSE;
-    }
-    // For any other character return TRUE so that the text gets added to the view
-    return TRUE;
-}
-
 - (void)dealloc {
     [theTextView release];
+    [venueId release];
+    [checkinView release];
+    [nonCheckinView release];
     [super dealloc];
 }
 
