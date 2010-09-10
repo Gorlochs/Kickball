5cefe15edf21bb1eb3864aef790a7301217f11a7
shawn <shawn@73bdbd85-dd75-0410-9d14-8527f93b1508>
2010-09-10 00:04:02 +0000

    fixed leak
    git-svn-id: http://literalshore.com/svn/kickball/trunk@1481 73bdbd85-dd75-0410-9d14-8527f93b1508

--- Classes/KBCreateTweetViewController.m
+++ Classes/KBCreateTweetViewController.m
@@ -206,9 +206,11 @@
 }
 
 -(void) uploadToFacebook{
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 	GraphAPI *graph = [[FacebookProxy instance] newGraph];
 	[graph postToWall:tweetTextView.text withImage:photoImage];	
 	[graph release];
+	[pool release];
 }
 
 - (void) submitToTwitter:(TweetPhotoResponse*)response {
